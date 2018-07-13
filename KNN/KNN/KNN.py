17c18927db8a25e15042f3dd56af4de0e41f9c2d
Ceekay-Shen <shendubo@me.com>
2018-07-13 23:11:25 +0800

    add some sources

--- KNN/KNN/KNN.py
+++ KNN/KNN/KNN.py
@@ -23,9 +23,29 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)
     return sortedClassCount[0][0]
 
+def file2matrix(filename):
+    fr = open(filename)
+    arraylines = fr.readlines()
+    numberOfLines = len(arraylines)
+    returnMat = zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+
+    for line in arraylines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+
+
 if __name__ == "__main__":
     group,labels = creatDataSet()
     a = classify0([1,2],group,labels,3)
     print(a)
+
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
 
     
